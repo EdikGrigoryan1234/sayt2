cdf64169674dcb5e1aadaa33bbb583065d7c2a3d
unknown <you@example.com>
2024-06-28 18:43:08 +0400

    save

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-let burger = document.querySelector('.burger_menu')
-let menu = document.querySelector('.burger_main')
-let open = false
+let burger = document.querySelector(".burger_menu");
+let menu = document.querySelector(".burger_main");
+let open = false;
 console.log(menu);
 
-burger.addEventListener('click',()=>{
-	if (open==false) {
-		menu.style.display='flex'
-		open=true
-	}else if(open==true){
-		menu.style.display='none'
-		open=false
-	}
-})
+burger.addEventListener("click", () => {
+  if (open == false) {
+    menu.style.display = "flex";
+    open = true;
+  } else if (open == true) {
+    menu.style.display = "none";
+    open = false;
+  }
+});
 console.log(burger);
